2f0c6bdc2a7078f7433262bd9ffd0d73ff88d6fb
looosemycoool <yhm19850415@gmail.com>
2024-06-02 02:54:05 +0900

    최종수정

--- 0528/models.py
+++ 0528/models.py
@@ -11,7 +11,8 @@ class Politician(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True)
     region = Column(String, index=True)
-    promises = relationship("Promise", back_populates="politician")
+    promises = relationship("Promise", back_populates="politician", cascade="all, delete-orphan")
+    posts = relationship("Post", back_populates="author", cascade="all, delete-orphan")
 
     def total_fulfillment_rate(self):
         total = len(self.promises)
@@ -28,6 +29,15 @@ class Promise(Base):
     description = Column(String, index=True)
     status = Column(String, default="확인 불가")
     politician = relationship("Politician", back_populates="promises")
+
+class Post(Base):
+    __tablename__ = "posts"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    content = Column(String)
+    author_id = Column(Integer, ForeignKey("politicians.id"))
+    author = relationship("Politician", back_populates="posts")
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
